07d6a2c4d4d457677b5f5bfad8efeff5b20467d8
Guillaume Ferron <guillaumef.22@gmail.com>
2017-10-08 19:47:53 +0200

    The state has a tuple base parameter to know where the base is

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from emulator import Emulator
 from random import randrange
+from dynamic_programming import DP
 
 # Initialization
-emule = Emulator()
-episodesnb = 100
-
-# Actual rendering
-emule.createmap()
-
-# Simulate the algorithm
-for i in range(episodesnb):
-    reward = emule.simulate(emule.firstState, emule.actions[randrange(0, len(emule.actions))])
-    emule.resetiterationnb()
-    print("**************")
+DP = DP()
+DP.generate_all_states()
 
 
 # End of simulation

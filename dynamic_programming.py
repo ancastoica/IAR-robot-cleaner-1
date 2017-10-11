3f256af740ad0710aa74cbeb602d735d2d5beb3b
Guillaume Ferron <guillaumef.22@gmail.com>
2017-10-12 00:21:13 +0200

    Working dynamic programming
    Need to refactor the room states parsing in because all states aren't fetched (some room configurations are missing)
    
    Need to test on an other computer, too many computation

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -2,18 +2,21 @@ from emulator import Emulator
 from state import State
 from robot import Robot
 from random import randrange
+from cell import Cell
 import api
 
 
 class DP:
+
     def __init__(self):
-        self.states = []    # all possible states
-        self.threshold = 0.01   # The threshold used to stop the interations
-        self.values = []    # The values of the states at t-1
+        self.states = []  # all possible states
+        self.values = []
+        self.threshold = 0.01  # The threshold used to stop the interations
 
     """
     generate all the possible states
     """
+
     def generate_all_states(self):
         for battery in range(0, 3):
             for robot_x in range(api.MAPSIZE):
@@ -26,9 +29,10 @@ class DP:
     """
     Generate a map using the given parameters
     """
-    def generate_all_map(self, battery, robot_x, robot_y, robot_orientation, base_x, base_y):
-        mapp = api.randommap(base_x, base_y)
 
+    def generate_all_map(self, battery, robot_x, robot_y, robot_orientation, base_x, base_y):
+        mapp = api.initmap(base_x, base_y)
+        tempmapp = mapp
         abs_battery = 100
         if battery == 0:
             abs_battery = 0
@@ -38,51 +42,92 @@ class DP:
             abs_battery = randrange(11, 100)
 
         robot = Robot(robot_x, robot_y, api.MAPSIZE, api.MAPSIZE, robot_orientation, abs_battery)
+
         self.states.append(State(robot, mapp, (base_x, base_y)))
+
+        for x in range(api.MAPSIZE):
+            for y in range(api.MAPSIZE):
+                # Focus on a cell
+                if x == base_x and y == base_y:
+                    # We consider the homebase cell as clean
+                    mapp[x][y] = Cell(x, y, 0, 1)
+                else:
+                    mapp[x][y] = Cell(x, y, 1, 0)
+                tempmapp = mapp
+                self.states.append(State(robot, mapp, (base_x, base_y)))
+
+                for x_prime in range(x, api.MAPSIZE):
+                    for y_prime in range(y, api.MAPSIZE):
+                        if x_prime == base_x and y_prime == base_y:
+                            # We consider the homebase cell as clean
+                            break
+                        mapp[x_prime][y_prime] = Cell(x_prime, y_prime, 1, 0)
+                        self.states.append(State(robot, mapp, (base_x, base_y)))
+                        mapp = tempmapp
+
+
 
     """
     Get the infinite norm of the difference of two vectors
     """
+
     def get_infinite_norme(self, values, values_prime):
-        maxvalue = 0
-        temp = 0
+        maxvalue = 0.0
+        temp = 0.0
         for i in range(len(values)):
             temp = abs(values[i] - values_prime[i])
             if temp > maxvalue:
                 maxvalue = temp
-        return max
+        return maxvalue
+
+    def get_value_function(self, emulator, state_ind):
+        rewards = [0.0 for i in range(len(api.ACTIONS))]
+        newstates = [[0.0, 0.0] for i in range(len(api.ACTIONS))]
+        probabilities = [0.0 for i in range(len(api.ACTIONS))]
+        q_values = [0.0 for i in range(len(api.ACTIONS))]
+
+        # Try each and every one of the possible actions
+        for action_ind in range(len(api.ACTIONS)):
+            # Get the rewards, states, probabilities as lists
+            rewards[action_ind], newstates[action_ind], probabilities[action_ind] = emulator.simulate(self.states[state_ind], api.ACTIONS[action_ind])
+            # Compute Q value
+            q_values[action_ind] = rewards[action_ind]
+
+            for possible_action in range(len(newstates[action_ind])):
+                possible_ind = self.state_exists(newstates[action_ind][possible_action])
+                q_values += api.DISCOUNTED_FACTOR * probabilities[action_ind] * self.get_value_function(emulator, possible_ind)
+
+        return max(q_values)
+
+    def state_exists(self, state):
+        index = 0
+        while index < len(self.states):
+            if state == self.states[index]:
+                return index
+            index += 1
+        return -1
 
     def run(self):
         # Initialization of the simulation
         self.generate_all_states()
-        self.values = [0 for i in range(len(self.states))]
+        self.values = [0.0 for i in range(len(self.states))]
+        values_prime = [0.0 for i in range(len(self.states))]
 
         emulator = Emulator("dynamic_programming")
-        rewards = [0 for i in range(len(api.ACTIONS))]
-        values = [0 for i in range(len(api.ACTIONS))]
-        newstates = [0 for i in range(len(api.ACTIONS))]
-        probabilities = [0 for i in range(len(api.ACTIONS))]
 
         while True:
             # Update the values at t-1 according to the values at t
-
-
+            values_prime = self.values[:]
             # Go through all the states
             for state_ind in range(len(self.states)):
-                # Try each and every one of the possible actions
-                for action_ind in range(len(api.ACTIONS)):
-                    # Get the rewards, states, probabilities as lists
-                    rewards[action_ind], newstates[action_ind], probabilities[action_ind] = emulator.simulate(self.states[state_ind], api.ACTIONS[action_ind])
-                    # Compute V
-                    values[action_ind] = rewards[action_ind] + api.DISCOUNTED_FACTOR * probabilities[action_ind] * self.values[state_ind]
 
-                # Update the new maximum value and the attached action
-                maximum_value = max(values)
-                optimal_action = api.ACTIONS[rewards.index(maximum_value)]
+                # Update the new maximum value
+                self.values[state_ind] = self.get_value_function(emulator, state_ind)
 
             # If the threshold is bigger than the difference between Vs and their predecessors, then we consider the algorithm as successful
-            # if self.get_infinite_norme(, self.values) < self.threshold:
-            break
+            if self.get_infinite_norme(self.values, values_prime) < self.threshold:
+                break
 
-        return
+        s0_index = self.state_exists(api.INITIAL_STATE)
 
+        return self.values[s0_index]

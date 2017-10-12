8c140eaf43ce94671368bdc588ba92fbad8aab4e
Guillaume Ferron <guillaumef.22@gmail.com>
2017-10-12 15:44:18 +0200

    New main with asking for the algorithm

--- main.py
+++ main.py
@@ -5,25 +5,64 @@ import matplotlib.pyplot as plt
 import api
 
 
-"""
-Dynamic Programming
-"""
-# Initialization
-DP = DP()
-v_s0 = DP.run()
-print("The performance of Dynamic Programming is : ", v_s0)
+algorithm_choice = ""
+while algorithm_choice != "DP" or algorithm_choice != "MC" or algorithm_choice != "QL" or algorithm_choice != "dp" or algorithm_choice != "mc" or algorithm_choice != "ql" or algorithm_choice != "all":
+    algorithm_choice = input("Choose the algorithm to execute (DP; MC; QL; all) : ")
 
-"""
-Monte Carlo
-"""
-MC = MC()
-q = []
+if algorithm_choice == "DP" or algorithm_choice == "dp":
+    """
+    Dynamic Programming
+    """
+    # Initialization
+    DP = DP()
+    v_s0 = DP.run()
+    print("The performance of Dynamic Programming is : ", v_s0)
 
-for t in range(10, 100):
-    v = MC.run(10000, t)
-    print(v)
-    q.append(v)
 
-plt.plot(q)
-plt.legend("Monte Carlo")
-plt.show()
+elif algorithm_choice == "MC" or algorithm_choice == "mc":
+    """
+    Monte Carlo
+    """
+    MC = MC()
+    q = []
+
+    for t in range(10, 100):
+        v = MC.run(10000, t)
+        print(v)
+        q.append(v)
+
+    plt.plot(q)
+    plt.legend("Monte Carlo")
+    plt.show()
+
+elif algorithm_choice == "QL" or algorithm_choice == "ql":
+    print("Q-Learning algorithm in construction, please wait for the developper !")
+
+elif algorithm_choice == "all":
+    """
+    Dynamic Programming
+    """
+    # Initialization
+    DP = DP()
+    v_s0 = DP.run()
+    print("The performance of Dynamic Programming is : ", v_s0)
+
+    """
+    Monte Carlo
+    """
+    MC = MC()
+    q = []
+
+    for t in range(10, 100):
+        v = MC.run(10000, t)
+        print(v)
+        q.append(v)
+
+    plt.plot(q)
+    plt.legend("Monte Carlo")
+    plt.show()
+
+    """
+    Q-Learning
+    """
+    print("Q-Learning algorithm in construction, please wait for the developper !")
